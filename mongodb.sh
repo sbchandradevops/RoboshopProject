e25715baeb67e65e7a0e14bcbd36433983718971
Bharath <sbchandradevops@gmail.com>
2023-10-21 06:57:35 +1100

    This is a Roboshop Project

--- mongodb.sh
+++ mongodb.sh
@@ -11,3 +11,10 @@ dnf install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
 
+#Usually MongoDB opens the port only to localhost(127.0.0.1), meaning this service can be accessed by the application that is hosted on this server only. However, we need to access this service to be accessed by another server, So we need to change the config accordingly.
+
+#Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+
+#Restart the service to make the changes effected.
+
+systemctl restart mongod
