ceb1f4e06562cb16f978f0f963a3bdf2e48c302c
Bharath <sbchandradevops@gmail.com>
2023-10-24 21:50:07 +1100

    Added Frontend.sh

---
+++ frontend.sh
@@ -0,0 +1,36 @@
+# 01-Frontend
+# The frontend is the service in RoboShop to serve the web content over Nginx. This will have the webframe for the web application.
+# This is a static content and to serve static content we need a web server. This server
+# Developer has chosen Nginx as a web server and thus we will install Nginx Web Server.
+# Install Nginx
+
+dnf install nginx -y
+
+# Start & Enable Nginx service
+
+systemctl enable nginx
+
+systemctl start nginx
+
+# Remove the default content that web server is serving.
+
+rm -rf /usr/share/nginx/html/*
+
+# Download the frontend content
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+# Extract the frontend content.
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip
+
+# Create Nginx Reverse Proxy Configuration.
+
+# Restart Nginx Service to load the changes of the configuration.
+
+systemctl restart nginx
+
+
+
